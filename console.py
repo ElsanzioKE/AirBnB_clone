9297ad2796aaec6c0d8965c16808c8a89fd46d04
ElSanzioKE <lesanjorelly@gmail.com>
2024-02-09 08:51:26 +0000

    fixed no 6

--- console.py
+++ console.py
@@ -1,19 +1,25 @@
 #!/usr/bin/python3
-"""This module imports cmd """
+"""
+    contains the entry point for the cmd
+    """
 import cmd
-class HBNBCommand(cmd.Cmd):
-    """Command interpreter class"""
 
+class HBNBCommand(cmd.Cmd):
+    """
+    command interpreter should implement:
+    EOF andquit the program
+    help(this action is provided by default by cmd but you
+    """
+    intro = ""
     prompt = "(hbnb)"
 
-    def do_quit(self, arg):
-        """Quit command to exit program"""
+    def do_quit(self, args):
+        """Quits command to exit the program"""
         return True
 
-    def do_EOF(self, arg):
-        """Exit the program with EOF"""
-        print("")
+    def do_EOF(self, args):
+        """Quit command to exit the program"""
         return True
 
-    if __name__ == '__main:__':
-        HBNBCommand().cmdloop()
+if __name__ == '__main__':
+       HBNBCommand().cmdloop()
